b7b83015649a68dbd7e81a894697400a0af3c97d
Oran Shuster <thristle666@gmail.com>
2019-01-12 16:19:25 +0200

    [Adapters] Add CC Glilot and other code formatting/type fixes

--- adapters/cinema_city/theatres.py
+++ adapters/cinema_city/theatres.py
@@ -1,43 +1,42 @@
 import logging
 from datetime import datetime
-from enum import Enum
-from typing import Tuple, List
+from typing import List, Tuple
 
 import requests
 from babel.dates import format_datetime
 
 from adapters.cinema_city import enums
-from adapters.common import TheatreAdapter, Event
-from adapters.enums import CompanyNames, CityNames, EventTags
+from adapters.common import Event, TheatreAdapter
+from adapters.enums import CityNames, CompanyNames, EventTags
 
 logger = logging.getLogger("CinemaCityTheatre")
 
 
 # noinspection PyRedundantParentheses
 class CinemaCityTheatre(TheatreAdapter):
-    def _parse_event_tags(self, event_name: str) -> Tuple[str, List[Enum]]:
+    def _parse_event_tags(self, event_name: str) -> Tuple[str, List[EventTags]]:
         # First check for hyphen versions
-        query_ind = event_name.find(enums.EventNameTagEndings.EnglishWithHyphen)
+        query_ind = event_name.find(enums.EventNameTagEndings.EnglishWithHyphen.value)
         if query_ind != -1:
             return (event_name[:query_ind], [EventTags.English])
 
-        query_ind = event_name.find(enums.EventNameTagEndings.HebrewWithHyphen)
+        query_ind = event_name.find(enums.EventNameTagEndings.HebrewWithHyphen.value)
         if query_ind != -1:
             return (event_name[:query_ind], [EventTags.Hebrew])
 
-        query_ind = event_name.find(enums.EventNameTagEndings.DubbedWithHyphen)
+        query_ind = event_name.find(enums.EventNameTagEndings.DubbedWithHyphen.value)
         if query_ind != -1:
             return (event_name[:query_ind], [EventTags.DubbedHebrew])
 
-        query_ind = event_name.find(enums.EventNameTagEndings.English)
+        query_ind = event_name.find(enums.EventNameTagEndings.English.value)
         if query_ind != -1:
             return event_name[:query_ind], [EventTags.English]
 
-        query_ind = event_name.find(enums.EventNameTagEndings.Hebrew)
+        query_ind = event_name.find(enums.EventNameTagEndings.Hebrew.value)
         if query_ind != -1:
             return (event_name[:query_ind], [EventTags.Hebrew])
 
-        query_ind = event_name.find(enums.EventNameTagEndings.Dubbed)
+        query_ind = event_name.find(enums.EventNameTagEndings.Dubbed.value)
         if query_ind != -1:
             return (event_name[:query_ind], [EventTags.DubbedHebrew])
 
@@ -55,9 +54,9 @@ class CinemaCityTheatre(TheatreAdapter):
     def _parse_event_list(self, event_list_from_server: list):
         parsed_event_list = []
         for event in event_list_from_server:
-            event_name = event[enums.EventListKeys.Name]
+            event_name = event[enums.EventListKeys.Name.value]
             event_datetime = datetime.strptime(
-                event[enums.EventListKeys.Dates][enums.EventListKeys.Date],
+                event[enums.EventListKeys.Dates.value][enums.EventListKeys.Date.value],
                 enums.EVENT_LIST_DATETIME_FORMAT,
             )
             event_name, tags = self._parse_event_tags(event_name=event_name)
@@ -89,4 +88,16 @@ class CinemaCityRishonLeZion(CinemaCityTheatre):
             theatre_id=self.THEATRE_ID,
             company=CompanyNames.CinemaCity,
             city=CityNames.RishonLeZion,
+        )
+
+
+class CinemaCityGlilot(CinemaCityTheatre):
+    THEATRE_ID = 1170
+
+    def __init__(self):
+        super(CinemaCityGlilot, self).__init__(
+            display_name="Cinema City Glilot",
+            theatre_id=self.THEATRE_ID,
+            company=CompanyNames.CinemaCity,
+            city=CityNames.Glilot,
         )
